6b690a5b64d301be9f44fc94f65f0774020718ff
CloarecTM <tm.cloarec@gmail.com>
2020-03-10 17:05:07 +0100

    Converter(login)

--- Authentification/src/main/java/com/fr/adaming/converter/UserConverter.java
+++ Authentification/src/main/java/com/fr/adaming/converter/UserConverter.java
@@ -1,5 +1,22 @@
 package com.fr.adaming.converter;
 
+import com.fr.adaming.dto.LoginDto;
+import com.fr.adaming.entity.User;
+
 public class UserConverter {
 
+	public User ConvertDtoToUser(LoginDto ldto) {
+		User u = new User();
+		u.setEmail(ldto.getEmail());
+		u.setPwd(ldto.getPwd());
+		return u;
+
+	}
+
+	public LoginDto ConvertUserToDto(User u) {
+		LoginDto ldto = new LoginDto();
+		ldto.setEmail(u.getEmail());
+		ldto.setPwd(u.getPwd());
+		return ldto;
+	}
 }
